0e5b4e9d2279a41c1132a2f898fe631ae27f741c
Fauzan Husain <achmadfauzan1340@gmail.com>
2023-05-18 20:07:23 +0800

    update detail history

--- pages/account/history/[id].js
+++ pages/account/history/[id].js
@@ -13,18 +13,18 @@ const DetailHistory = ({ confirmationDetail, user }) => {
             <title>Fight camp || History detail</title>
         </Head> 
 
-        <div className="container px-20 text-white m-auto">
-            <h1 className="text-3xl font-semibold tracking-wide">Detail History</h1>
+        <div className="container px-3 md:px-5 lg:px-20 text-white m-auto">
+            <h1 className="text-2xl md:text-3xl font-semibold tracking-wide">Detail History</h1>
             {confirmationDetail.map((item) => {
                 return (
                     <>
                         <Image className="w-full h-[170px] object-cover mt-7 rounded-sm" src="/img/gambar-training.jpg" width={400} height={0} alt="gambar-training" />
 
-                        <div className="grid grid-cols-2 gap-5 mt-5">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
                             <div>
                                 <div className="flex justify-between">
                                     <div>
-                                        <h1 className="font-semibold text-2xl tracking-wide">{item.historyTraining.trainingName}</h1>
+                                        <h1 className="font-semibold text-xl md:text-2xl tracking-wide">{item.historyTraining.trainingName}</h1>
                                         <div className="flex">
                                             <p className="text-sm mt-4 font-semibold text-white/60"><NumericFormat prefix="Rp. " value={item.historyTraining.price} displayType="text" thousandSeparator="." decimalSeparator="," />{' '} -</p>
                                             {item.historyDiscount && item.historyDiscount.discount && (
@@ -36,7 +36,7 @@ const DetailHistory = ({ confirmationDetail, user }) => {
                                     </div>
 
                                     <div>
-                                        <h1 className="bg-red-600 p-2 font-semibold rounded-md text-sm">{item.historyTraining.package}</h1>
+                                        <h1 className="bg-red-600 cursor-pointer p-2 font-semibold rounded-md text-sm">{item.historyTraining.package}</h1>
                                         <h1 className="text-sm mt-4 font-semibold text-white/60">{item.historyTraining.session}</h1>
                                     </div>
                                 </div>
@@ -60,7 +60,7 @@ const DetailHistory = ({ confirmationDetail, user }) => {
                             </div>
                         </div>
 
-                        <div className="grid grid-cols-2 gap-5 mt-8">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-8">
                             <div className="flex justify-between">
                                 <div>
                                     <h1 className="text-sm font-semibold">Nomor whatsapp admin</h1>
@@ -72,6 +72,8 @@ const DetailHistory = ({ confirmationDetail, user }) => {
                                 </div>
                             </div>
 
+                            <hr className="md:hidden" />
+
                             <div>
                                 <h1 className="text-sm font-semibold">{user.username}</h1>
                                 <p className="text-sm mt-3 font-semibold text-white/60">{user.email}</p>
@@ -80,17 +82,17 @@ const DetailHistory = ({ confirmationDetail, user }) => {
 
                         <div className="mt-14">
                         {item.status === "pending" && (
-                            <h1 className="text-sm text-center w-full bg-orange-500 p-3 font-semibold">
+                            <h1 className="text-sm text-center cursor-pointer w-full bg-orange-500 hover:bg-orange-600 transition-all duration-400 p-3 font-semibold">
                                 Pending
                             </h1>
                         )}
                         {item.status === "success" && (
-                            <h1 className="text-sm text-center w-full bg-green-700 p-3 font-semibold">
+                            <h1 className="text-sm text-center cursor-pointer w-full bg-green-700 hover:bg-green-800 transition-all duration-400 p-3 font-semibold">
                                 Success
                             </h1>
                         )}
                         {item.status === "failed" && (
-                            <h1 className="text-sm text-center w-full bg-red-700 p-3 font-semibold">
+                            <h1 className="text-sm text-center cursor-pointer w-full bg-red-700 hover:bg-red-800 transition-all duration-400 p-3 font-semibold">
                                 Failed
                             </h1>
                         )}
